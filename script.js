63ef3b18f85cc8aa5e28185f8ecd30fbe6f5bba6
Urvino <castelanu91@gmail.com>
2023-10-10 13:28:28 -0700

    added comments and removed unessary code

--- script.js
+++ script.js
@@ -2,69 +2,57 @@
 function getComputerChoice() {
   //i: random number to between 3 choices out of 10...
   //o: random string, with either "rock" , 'paper' or 'scissors'
-  //declare a random variable to get a random number 
-  // let random = Math.floor(Math.random() * 4);
-  // // if else to check if number is greater than 6
-  //   // if so return 'rock'
-  // if(random === 3) {
-  //   return 'rock';
-  //   // else if number is greater than 2 but less than 3
-  //   //return 'paper'
-  // } else if (random > 1 && random < 3) {
-  //   return 'paper';
-  //    //else return 'scissors'
-  // } else {
-  //   return 'scissors';
-  // }
-  //OR
+  //declare an array to keep track of 3 choices. 
   let choices = ['Rock', 'Paper', 'Scissors'];
+  // declare a variable to get a random choice from the array of choices.
   const choice = choices[Math.floor(Math.random() * choices.length)];
+  // return that choice to lowerCase.
   return choice.toLowerCase();
 }
 
-function game() {
+// function game() {
 
-  //establish a round count,playerWin count, computerWin count
-  let round = 0;
-  let playerWin = 0;
-  let computerWin = 0;
+//   //establish a round count,playerWin count, computerWin count
+//   let round = 0;
+//   let playerWin = 0;
+//   let computerWin = 0;
 
-  //use a loop. either for loop or while loop.
-  while (round < 5) {
-    //when invoked increment round by one.
-    round++;
-    const playerSelection = prompt("Rock, Paper, Scissors?");
+//   //use a loop. either for loop or while loop.
+//   while (round < 5) {
+//     //when invoked increment round by one.
+//     round++;
+//     const playerSelection = prompt("Rock, Paper, Scissors?");
     
-    if (playerSelection === null || playerSelection === undefined || playerSelection === '' || (playerSelection.toLowerCase() !== 'rock' && playerSelection.toLowerCase() !== 'paper' && playerSelection.toLowerCase() !== 'scissors')) {
-        console.log('Select rock, paper or scissors. Refresh to play again..');
-        return;
-    } 
+//     if (playerSelection === null || playerSelection === undefined || playerSelection === '' || (playerSelection.toLowerCase() !== 'rock' && playerSelection.toLowerCase() !== 'paper' && playerSelection.toLowerCase() !== 'scissors')) {
+//         console.log('Select rock, paper or scissors. Refresh to play again..');
+//         return;
+//     } 
 
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection)); 
-    // make playerSelection function parameter case sensitive can be any rock, ROCK, or RocK variation.
-    console.log("Player wins " + playerWin);
-    console.log("Computer wins " + computerWin);
+//     const computerSelection = getComputerChoice();
+//     console.log(playRound(playerSelection, computerSelection)); 
+//     // make playerSelection function parameter case sensitive can be any rock, ROCK, or RocK variation.
+//     console.log("Player wins " + playerWin);
+//     console.log("Computer wins " + computerWin);
 
-    // function that plays a single round.
-    function playRound(playerSelection, computerSelection) {
-      //i: 2 parameters, playerSeletion and computerSelection
-      //o: return winner of the round as a string. 
-      //if else to check if player won or computer won
-        //return appropiate string winner.
-      //convert the player choice to lower case.
-      const player = playerSelection.toLowerCase();
-      if((player === 'rock' && computerSelection === 'paper') || (player === 'paper' && computerSelection === 'scissors') || (player === 'scissors' && computerSelection === 'rock')) {
-        computerWin++;
-        return "You Lose! Computer won.";
-      } else if ((player === 'paper' && computerSelection === 'rock') || (player === 'rock' && computerSelection === 'scissors') || (player === 'scissors' && computerSelection === 'paper')) {
-        playerWin++;
-        return 'You win!'
-      } else {
-        return "It's a tie."
-      }
-    }
-  }
+//     // function that plays a single round.
+//     function playRound(playerSelection, computerSelection) {
+//       //i: 2 parameters, playerSeletion and computerSelection
+//       //o: return winner of the round as a string. 
+//       //if else to check if player won or computer won
+//         //return appropiate string winner.
+//       //convert the player choice to lower case.
+//       const player = playerSelection.toLowerCase();
+//       if((player === 'rock' && computerSelection === 'paper') || (player === 'paper' && computerSelection === 'scissors') || (player === 'scissors' && computerSelection === 'rock')) {
+//         computerWin++;
+//         return "You Lose! Computer won.";
+//       } else if ((player === 'paper' && computerSelection === 'rock') || (player === 'rock' && computerSelection === 'scissors') || (player === 'scissors' && computerSelection === 'paper')) {
+//         playerWin++;
+//         return 'You win!'
+//       } else {
+//         return "It's a tie."
+//       }
+//     }
+//   }
 
   // if game is over tally wins.
   if (playerWin === computerWin) {

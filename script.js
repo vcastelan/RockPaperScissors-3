3e2fb83b84692e6a3f963f6bfb39837427dc970f
Urvino <castelanu91@gmail.com>
2023-10-10 17:26:39 -0700

    finished script on game functionality

--- script.js
+++ script.js
@@ -3,36 +3,41 @@ let round = 0;
 let playerWin = 0;
 let computerWin = 0;
 
-let computerSelection;
+// declare a variable to make computer and player selections
+let computerChoice;
 let playerChoice;
 
+// output player score using DOM
 const player = document.querySelector("#player-win");
 player.textContent = `Player Score: ${playerWin}`;
 
+// output computer score using DOM
 const computer = document.querySelector("#comp-win");
 computer.textContent = `Computer Score: ${computerWin}`;
 
-// div output p.
+// div output results using DOM
 const output = document.querySelector('#output');
 output.textContent = "Rock, Paper, Scissors?"
 
 const buttons = document.querySelectorAll('.btn');
-// we use the .forEach method to iterate through each button
-buttons.forEach((button) => {
-  // and for each one we add a 'click' listener
-  button.addEventListener('click', () => {
-    playerChoice = button.id;
-    if (playerChoice === "rock"){
-      playerChoice = 'rock';
-    } else if(playerChoice === 'paper') {
-      playerChoice = 'paper';
-    } else {
-      playerChoice = 'scissors';
-    }
-    computerSelection = getComputerChoice();
-    playRound(playerChoice, computerSelection);
-  });
-});
+    // we use the .forEach method to iterate through each button
+    buttons.forEach((button) => {
+      // and for each one we add a 'click' listener to select the player choice.
+      button.addEventListener('click', () => {
+        playerChoice = button.id;
+        if (playerChoice === "rock"){
+          playerChoice = 'rock';
+        } else if(playerChoice === 'paper') {
+          playerChoice = 'paper';
+        } else {
+          playerChoice = 'scissors';
+        }
+        // get computer choice
+        computerChoice = getComputerChoice();
+        //run playround function passing in playerChoice and computerChoice.
+        game();
+      });
+    });
 
 // i: function to get computer choice
 function getComputerChoice() {
@@ -47,59 +52,41 @@ function getComputerChoice() {
 }
 
 // function that plays a single round.
-function playRound(playerSelection, computerSelection) {
-//i: 2 parameters, playerSeletion and computerSelection
-//o: return winner of the round as a string. 
-//if else to check if player won or computer won
-  //return appropiate string winner.
-  if((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'rock')) {
-    computerWin++;
+function playRound(playerChoice, computerChoice) {
+  //i: 2 parameters, playerSeletion and computerSelection
+  //o: return winner of the round as a string. 
+  //if else to check if player won or computer won
+    //return appropiate string winner.
+  if((playerChoice === 'rock' && computerChoice === 'paper') || (playerChoice === 'paper' && computerChoice === 'scissors') || (playerChoice === 'scissors' && computerChoice === 'rock')) {
     output.textContent = "You Lose! Computer won.";
-  } else if ((playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-    playerWin++;
+    computerWin++;
+  } else if ((playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'scissors' && computerChoice === 'paper')) {
     output.textContent = 'You win!';
+    playerWin++;
   } else {
     output.textContent = "It's a tie.";
   }
 }
 
-// function game() {
-
-// // establish a round count,playerWin count, computerWin count
-
-//   //use a loop. either for loop or while loop.
-//   while (round < 5) {
-//     //when invoked increment round by one.
-//     round++;
-//     const playerSelection = prompt("Rock, Paper, Scissors?");
-    
-//     if (playerSelection === null || playerSelection === undefined || playerSelection === '' || (playerSelection.toLowerCase() !== 'rock' && playerSelection.toLowerCase() !== 'paper' && playerSelection.toLowerCase() !== 'scissors')) {
-//         console.log('Select rock, paper or scissors. Refresh to play again..');
-//         return;
-//     } 
-
-//     let computerSelection;
-//     // console.log(playRound(playerSelection, computerSelection)); 
-//     // // make playerSelection function parameter case sensitive can be any rock, ROCK, or RocK variation.
-//     // console.log("Player wins " + playerWin);
-//     // console.log("Computer wins " + computerWin);
-//   }
-// }
-
-  // if game is over tally wins.
-  // if (playerWin === computerWin) {
-  //   console.log('Tie game, run it again?');
-  //   console.log("Player wins totals " + playerWin);
-  //   console.log("Computer wins totals " + computerWin);
-  // } else if (playerWin > computerWin) {
-  //   console.log("You did it!! Congrats, you won!!!");
-  //   console.log("Player wins totals " + playerWin);
-  //   console.log("Computer wins totals " + computerWin);
-  // } else {
-  //   console.log("Game over. You Lost!");
-  //   console.log("Player wins totals " + playerWin);
-  //   console.log("Computer wins totals " + computerWin);
-  // }
-// }
-
-// game();
+function game() {
+  //use a loop
+    player.textContent = `Player Score: ${playerWin}`;
+    computer.textContent = `Computer Score: ${computerWin}`;
+    playRound(playerChoice, computerChoice);
+    // if game is over tally wins.
+    if (computerWin === 5) {
+      output.textContent = "Game over. You Lost!";
+      player.textContent = "Player wins totals " + playerWin;
+      computer.textContent = "Computer wins totals " + computerWin;
+      round = 0;
+      computerWin = 0;
+      playerWin = 0;
+    } else if (playerWin === 5) {
+      output.textContent = "You did it!! Congrats, you won!!!";
+      player.textContent = "Player wins totals " + playerWin;
+      computer.textContent = "Computer wins totals " + computerWin;
+      round = 0;
+      computerWin = 0;
+      playerWin = 0;
+    } 
+}
